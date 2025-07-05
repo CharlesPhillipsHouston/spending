991b0adf4f1d85c13f74b8d584a57f1e17f65ce4
Charles Phillips <charles@spaceflightresearch.net>
2025-07-05 16:15:23 -0500

    does run, does read from external file, builds external file but does not write to it yet

--- spending/main.cpp
+++ spending/main.cpp
@@ -1,9 +1,9 @@
 //
 //  main.cpp
 //  spending - a test application to look at spending and practice C++
-//  3 jul 2025 doesn't run yet :-(
+//  5 jul 2025 1615  runs good
 //  Created by Charles Phillips on 28 jun 2025
-//  currently does NOT read file, it did grrrr
+//  currently does read file
 
 #include <iostream>
 #include <fstream>
@@ -26,21 +26,26 @@ int main(int argc, const char * argv[])
 string homeDir = getenv("HOME");
     cout<< "in main"<<endl;
 
-string ifname = homeDir + "/Desktop/common_files/inputs.rtf";
+string ifname = homeDir + "/Desktop/common_files/inputs";
 ifstream fin(ifname);
     
-string ofname = homeDir + "/Desktop/common_files/outputs.rtf";
+string ofname = homeDir + "/Desktop/common_files/outputs";
     
 ofstream fout(ofname);
     int i=0; // prepare to count in the while loop
 
-    while (!fin.eof())
+ //   while (!fin.eof())
+    while (i<2)    // want to read to end of file
+    
     {
         getline(fin, line);
         cout<<"line"<<line<<endl;
         
         string location = line.substr(0,10);
-        cout<<"   location"<<  location<<endl;
+        cout<<"   location"<<  location<<endl;  // output to "console" ie screen
+        fout<<"  line"<< line << endl;  // funny that endl() isn't accepted
+        fout<<"  location"<<  location<< endl;  // output to file?
+        
       //  fout<<" location"<< location<<endl;   // builds really big file, infinite loop
         ++i; // increment count
         
