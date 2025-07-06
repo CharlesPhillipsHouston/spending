cd84dba0b8e6c603086fb1b81e555a99d3879704
Charles Phillips <charles@spaceflightresearch.net>
2025-07-06 18:01:45 -0500

    gets "line" in code, does not read file. outputs ok.

--- spending/main.cpp
+++ spending/main.cpp
@@ -1,7 +1,7 @@
 //
 //  main.cpp
 //  spending - a test application to look at spending and practice C++
-//  5 jul 2025 1620  runs good
+//  5 jul 2025 2015  runs good
 //  Created by Charles Phillips on 28 jun 2025
 //  currently does read file and outputs to file
 
@@ -18,39 +18,45 @@ string year;
 string cost;
 string type;
 
-string line="g"; // get a line of text and then scan for fields
+
 
 int main(int argc, const char * argv[]) 
 
 {
-string homeDir = getenv("HOME");
+    string line="ggggggg"; // get a line of text and then scan for fields
+    
+    string homeDir = getenv("HOME");
     cout<< "in main"<<endl;  // debug step
-
-string ifname = homeDir + "/Desktop/common_files/inputs";
-ifstream fin(ifname);
     
-string ofname = homeDir + "/Desktop/common_files/outputs";
     
-ofstream fout(ofname);
+    string ifname = homeDir + "/Desktop/common_files/inputs";
+    ifstream fin(ifname);
+    
+    string ofname = homeDir + "/Desktop/common_files/outputs";
+    
+    ofstream fout(ofname);
     int i=0; // prepare to count in the while loop
-
-    while (!fin.eof())   // this produces infinite loop
-//    while (i<2)    // want to read to end of file! This does work
+    
+       while (!fin.eof())   // this works
     
     {
-        getline(fin, line);  // alternative to fin.getline below
+        {
         
-        cout<<"line  "<<  line<<endl;
-        
-        string location = line.substr(0,10);
-        cout<<"   location  "<<  location<<endl;  // output to "console" ie screen
-        fout<<"  line   "<< line << endl;  // funny that endl() isn't accepted
-        fout<<"  location  "<<  location<< endl;  // output to file
-        
-        ++i; // increment count
-        
-    }
-    fin.close();
-    fout.close();
-    return 0;
-}
+            getline(fin, line);  // does not get the line on this step
+            
+            cout<<"line  "<<  line<<endl;
+            
+            string location = line.substr(0,3);  // does not read the input file!
+            
+            cout<<"   location  "<<  location<<endl;  // output to "console" ie screen
+            
+            fout<<"  line   "<< line << endl;  // funny that endl() isn't accepted
+            fout<<"  location  "<<  location<< endl;  // output to file
+            
+            ++i; // increment count
+            
+        }
+        fin.close();
+        fout.close();
+        return 0;
+    }}
