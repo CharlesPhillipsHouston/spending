10f21c7a9e6483be2f3a38e699691b3f6b551824
Charles Phillips <charles@spaceflightresearch.net>
2025-07-07 17:57:48 -0500

    added another while to read file.

--- spending/main.cpp
+++ spending/main.cpp
@@ -1,7 +1,7 @@
 //
 //  main.cpp
-//  spending - a test application to look at spending and practice C++
-//  5 jul 2025 2015  runs good
+//  spending - a test application to look at spending and practice C++ input/output
+//  7 jul 2025 1800  runs good
 //  Created by Charles Phillips on 28 jun 2025
 //  currently does read file and outputs to file
 
@@ -11,24 +11,22 @@
 
 using namespace std;
 
-string location="a"; // set up string + initial value
-string day="b";
-string month="c";
-string year;
-string cost;
-string type;
-
-
 
 int main(int argc, const char * argv[]) 
 
 {
-    string line="ggggggg"; // get a line of text and then scan for fields
+    string location="a"; // set up string + initial value
+    string day="b";
+    string month="c";
+    string year;
+    string cost;
+    string type;
+    
+    string line="aaaaaaaaaaaaaa"; // get a line of text and then scan for fields
     
     string homeDir = getenv("HOME");
     cout<< "in main"<<endl;  // debug step
-    
-    
+
     string ifname = homeDir + "/Desktop/common_files/inputs";
     ifstream fin(ifname);
     
@@ -37,20 +35,20 @@ int main(int argc, const char * argv[])
     ofstream fout(ofname);
     int i=0; // prepare to count in the while loop
     
-       while (!fin.eof())   // this works
+       while (!fin.eof())   // this works but we only output one line?
     
     {
         {
         
-            getline(fin, line);  // does not get the line on this step
+            while (getline(fin, line));  // does not get the line on this step
             
-            cout<<"line  "<<  line<<endl;
+        cout<<"line  "<<  line<<endl;
             
-            string location = line.substr(0,3);  // does not read the input file!
+            string location = line.substr(0,6);  // does not read the input file!
             
             cout<<"   location  "<<  location<<endl;  // output to "console" ie screen
-            
-            fout<<"  line   "<< line << endl;  // funny that endl() isn't accepted
+            cout<<" i="<< i<<endl;
+       //     fout<<"  line   "<< line << endl;  // funny that endl() isn't accepted
             fout<<"  location  "<<  location<< endl;  // output to file
             
             ++i; // increment count
